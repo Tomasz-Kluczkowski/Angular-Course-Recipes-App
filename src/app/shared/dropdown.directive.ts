1fd1cdf2d6e764428e7fcf98ed59ae3c39418ee3
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2019-07-29 23:03:19 +0100

    Add dropdown directive and use it.

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,19 +1,20 @@
-import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-  dropdownOpened = false;
+  // dropdownOpened = false;
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
-
+  @HostBinding('class.open') isOpen = false;
 
   @HostListener('click') onDropDownClicked(eventData: Event) {
-    const parentNode = this.elementRef.nativeElement.parentNode;
-    this.renderer[this.dropdownOpened ? 'removeClass' : 'addClass'](parentNode, 'open');
-    this.dropdownOpened = !this.dropdownOpened;
+    // const parentNode = this.elementRef.nativeElement.parentNode;
+    // this.renderer[this.dropdownOpened ? 'removeClass' : 'addClass'](parentNode, 'open');
+    // this.dropdownOpened = !this.dropdownOpened;
     // this.elementRef.nativeElement.parentNode.classList.toggle('open');
+    this.isOpen = !this.isOpen;
   }
 }
