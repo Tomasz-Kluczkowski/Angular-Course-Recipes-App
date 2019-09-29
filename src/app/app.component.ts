2166801b0394a24bffec7d4b5736a238b2ca2eec
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2019-09-29 16:17:19 +0100

    Use subjects in services. Remove unused methods.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RECIPES';
-  showMenuElements = {recipesVisible: true, shoppingListVisible: false};
-
-  showMenu(event) {
-    this.showMenuElements = event;
-  }
 }
